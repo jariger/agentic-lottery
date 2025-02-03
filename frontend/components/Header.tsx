83abfb8fe85120cde52de222aaaa7ba58ef0785c
jariger <jariger2004@gmail.com>
2025-02-03 18:07:14 -0500

    Added a connect wallet button

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Link from "next/link";
-
+import { ConnectButton } from "thirdweb/react"; 
+import { createThirdwebClient } from "thirdweb";
+import { lightTheme } from "thirdweb/react";
+const client = createThirdwebClient({
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+} as any);
 const Header: React.FC = () => {
   return (
     <nav className="w-full bg-gray-900 text-white p-4 flex justify-between items-center">
@@ -12,6 +17,21 @@ const Header: React.FC = () => {
         <Link href="/claim">
           <span className="cursor-pointer hover:text-gray-400">Claim Prize</span>
         </Link>
+        <ConnectButton
+        client={client} 
+        theme={lightTheme({
+          colors: {
+            primaryButtonBg: "#4084f4", // Change button background color
+            primaryButtonText: "#ffffff", // Change button text color
+            secondaryButtonBg: "#2864ec", // Background color for secondary states
+            secondaryButtonText: "#FFFFFF", // Text color for secondary button
+          },
+        })}
+        connectButton={{
+          label: "Connect Wallet", // Change button label
+          className: "px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all shadow-md",
+        }}
+        />
       </div>
     </nav>
   );
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider, ConnectButton} from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+
+const activeChain = "ethereum"
+const client = createThirdwebClient({
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+} as any);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return( 
+    <ThirdwebProvider>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+);
 }
